f424ee9b0120d7d3e81ad40ea927546b3d873015
Ridho Febriansa <ridho.febriansa@zero-one-group.com>
2022-04-05 07:45:10 +0700

    end of chapter one

--- apps/bookstore/src/app/app.tsx
+++ apps/bookstore/src/app/app.tsx
@@ -1,14 +1,13 @@
 import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+const StyledApp = styled.div``;
 
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="bookstore" />
+      <header>
+        <h1>Bookstore</h1>
+      </header>
     </StyledApp>
   );
 }
